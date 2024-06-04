87432c3be8b7f9f9c60a5cf38c0bbcd3070a8924
Ramim Siddiqui <ramimalisiddiqui0@gmail.com>
2024-06-04 10:15:03 +0500

    create footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <footer style={footerStyle}>
+            <div className="container">
+                 <div className="row text-center">
+                     <div className="col-md-12 col-lg-12 col-sm-12">
+                        <p>&copy; 2024 The Future Power . All Rights Reserved.</p>       
+                    </div>
+                </div>
+            </div>
+       </footer>
+    </>
   )
 }
 
 export default Footer
+
+const footerStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0)",
+  color: 'black',
+  textAlign: 'center',
+  padding: '0px',
+  position: 'fixed',
+  bottom: '0',
+  width: '100%',
+  fontSize: '20px',
+  fontWeight: 'bold',
+  fontFamily: 'Poppins',
+};
