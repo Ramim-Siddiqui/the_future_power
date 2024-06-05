a76d8b10693f8dfe7b88a31241ec1dda80e61ce8
Ramim Siddiqui <ramimalisiddiqui0@gmail.com>
2024-06-05 08:28:49 +0500

    update footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,22 @@ import Routing from "./components/Routing";
 
 
 function App() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audio = new Audio(welcomeaudio);
+    audioRef.current = audio;
+
+    audio.play()
+      .catch(error => {
+        console.error('Error playing audio:', error);
+      });
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
   return (
     <>
       <Routing />
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,32 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faClock, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
   return (
-    <>
-      <footer style={footerStyle}>
-            <div className="container">
-                 <div className="row text-center">
-                     <div className="col-md-12 col-lg-12 col-sm-12">
-                        <p>&copy; 2024 The Future Power . All Rights Reserved.</p>       
-                    </div>
-                </div>
-            </div>
-       </footer>
-    </>
-  )
-}
+    <footer className="text-center text-lg-start" style={footerStyle}>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+            <h5 className="text-uppercase">Contact</h5>
+            <ul className="list-unstyled mb-0">
+              <li className="mb-2">
+                <FontAwesomeIcon icon={faClock} className="me-2" />
+                Mon - Sat : 10.00 AM - 07.00 PM
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
+                A-6, Block 13-B, Gulshan-e-Iqbal, Karachi, Sindh, Pakistan
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-4 col-md-6 mb-4 mb-lg-0 d-flex align-items-center justify-content-center">
+            <p className="m-0">&copy; 2024 The Future Power. All Rights Reserved.</p>
+          </div>
+          <div className="col-lg-4 col-md-12 d-flex justify-content-lg-end justify-content-md-center justify-content-center align-items-center">
+            <a href="https://www.facebook.com/RamimAli2003" target="_blank" rel="noopener noreferrer" className="me-4 text-reset">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/ramimali2003?igsh=MW16NGd6ZHkwcjJvcQ==" target="_blank" rel="noopener noreferrer" className="me-4 text-reset">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/ramim-siddiqui-910404235" target="_blank" rel="noopener noreferrer" className="text-reset">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
 
 const footerStyle = {
-  backgroundColor: "white",
-  color: 'black',
-  textAlign: 'center',
-  padding: '0px',
-  position: 'relative',
-  bottom: '0',
-  width: '100%',
-  fontSize: '4vh',
-  fontWeight: 'bold',
+  backgroundColor: '#002244', 
+  color: 'white', 
+  fontSize: '2.5vh',
   fontFamily: 'Poppins',
 };
